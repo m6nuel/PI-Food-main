f2ebff7074cfbf77d09c161b308228d16b440796
jesus <m6nuel@gmail.com>
2022-11-02 08:33:05 -0300

    Rutas asignadas

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,33 @@
 import './App.css';
+import { BrowserRouter as Router,
+   Redirect,
+   Route,
+   Switch } from "react-router-dom"
+import { LandingPage } from './components/LandingPage';
+import { Home } from './components/Home';
+import { Detail } from './components/Detail';
+import { Create } from './components/Create';
 
 function App() {
   return (
     <div className="App">
-      <h1>Henry Food</h1>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+            <LandingPage />
+          </Route>
+          <Route path="/home">
+            <Home />
+          </Route>
+          <Route path="/detail/:id">
+            <Detail />
+          </Route>
+          <Route path="/create">
+            <Create />
+          </Route>
+          <Redirect to="/" />
+        </Switch>
+      </Router>
     </div>
   );
 }
