7687702d733ad1a3f4dc39e8e50a86eea65d6c30
jesus <m6nuel@gmail.com>
2022-11-14 20:28:45 -0300

    Validacion del Formulario

--- client/src/components/Create/index.js
+++ client/src/components/Create/index.js
@@ -3,6 +3,17 @@ import { useDispatch, useSelector } from 'react-redux'
 import { createRecipe } from '../../redux/actions';
 import style from './form.module.css';
 
+const validate = ( form ) => {
+  let errors = {};
+  if (!form.nombre) {
+    errors.nombre = 'El nombre de la receta es necesario'
+  }
+  if (!form.resumen) {
+    errors.resumen = 'El Resumen de la receta es Necesario'
+  }
+  return errors;
+}
+
 export const Create = () => {
   
   const dispatch = useDispatch();
@@ -16,11 +27,20 @@ export const Create = () => {
     dieta: []
   })
 
+  const [errors, setErrors] = useState({
+    nombre: '',
+    resumen: ''
+  })
+
   const handleChange = (e) => {
     setForm({
       ...form,
       [e.target.name]: e.target.value
-    })
+    });
+    setErrors( validate({
+      ...form,
+      [e.target.name] : e.target.value
+    }))
   }
 
   const handleSelect = (e) => {
@@ -52,25 +72,25 @@ export const Create = () => {
         <div className={`${ style.form_item }`}>
           <input type='text' value={form.nombre} name='nombre' onChange={ handleChange } required/>
           <label> Nombre de la Receta: </label>
-          <div></div>
+          <div className={`${ style.error }`}> { errors.nombre && errors.nombre } </div>
         </div>
 
         <div className={`${ style.form_item }`}>
           <input type='text' value={  form.resumen } name='resumen' onChange={ handleChange } required/>
           <label> Resumen del Plato: </label>
-          <div></div>
+          <div className={`${ style.error }`}> { errors.resumen && errors.resumen } </div>
         </div>
 
         <div className={`${ style.form_item }`}>
           <input type='text' value={  form.nivelDeComida } name='nivelDeComida' onChange={ handleChange } required/>
           <label> Nivel de comida: </label>
-          <div></div>
+          {/* <div className={`${ style.error }`}>nombre de la Receta</div> */}
         </div>
 
         <div className={`${ style.form_item }`}>
           <input type='text' value={  form.pasoAPaso } name='pasoAPaso' onChange={ handleChange } required/>
           <label> Paso a Paso: </label>
-          <div></div>
+          {/* <div className={`${ style.error }`}>nombre de la Receta</div> */}
         </div>
 
         <div>
